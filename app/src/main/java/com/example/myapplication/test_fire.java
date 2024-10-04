7c9368b7050f579609d37021e083eb73cf5435d3
Kujek <kayfolk555@gmail.com>
2024-10-04 08:20:02 +0700

    finished project

---
+++ app/src/main/java/com/example/myapplication/test_fire.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class test_fire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_test_fire);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        ImageView fireImage = findViewById(R.id.fire); // ระบุ ID ของรูป
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fire_animation);
+        fireImage.startAnimation(animation);
+    }
+}
