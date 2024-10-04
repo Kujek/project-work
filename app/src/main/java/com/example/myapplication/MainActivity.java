230a53dbfaa2201908b09d16a1e45ed991cd7e7f
Kujek <kayfolk555@gmail.com>
2024-10-04 23:50:39 +0700

    finished project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,7 +16,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private int SelectedTab = 1;
+    private int selectedTab = 1;
 
 
 
@@ -43,11 +45,49 @@ public class MainActivity extends AppCompatActivity {
         final TextView bmiText = findViewById(R.id.text_bmi);
 
 
+        getSupportFragmentManager().beginTransaction()
+                        .setReorderingAllowed(true)
+                        .replace(R.id.fragmeentContainer, HomeFragment.class, null)
+                                .commit();
+
+
+
+
 
         homeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if()
+                if(selectedTab != 1){
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.fragmeentContainer, HomeFragment.class, null)
+                            .commit();
+                    foodText.setVisibility(View.GONE);
+                    bmiText.setVisibility(View.GONE);
+
+                    foodImage.setImageResource(R.drawable.icon_food);
+                    bmiImage.setImageResource(R.drawable.icon_bmi);
+
+
+                    foodLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    bmiLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+
+                    homeText.setVisibility(View.VISIBLE);
+                    homeImage.setImageResource(R.drawable.like_icon_home);
+                    homeLayout.setBackgroundResource(R.drawable.round_back_home);
+
+
+                    ScaleAnimation scaleAnimation = new ScaleAnimation(0.8f,1.0f,1f,1f, Animation.RELATIVE_TO_SELF,0.0f,Animation.RELATIVE_TO_SELF,0.0f);
+                    scaleAnimation.setDuration(200);
+                    scaleAnimation.setFillAfter(true);
+                    homeLayout.startAnimation(scaleAnimation);
+
+
+                    selectedTab = 1;
+
+                }
+
 
             }
         });
@@ -56,12 +96,73 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                if(selectedTab != 2){
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.fragmeentContainer, FoodFragment.class, null)
+                            .commit();
+                    homeText.setVisibility(View.GONE);
+                    bmiText.setVisibility(View.GONE);
+
+                    homeImage.setImageResource(R.drawable.icon_home);
+                    bmiImage.setImageResource(R.drawable.icon_bmi);
+
+
+                    homeLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    bmiLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+
+                    foodText.setVisibility(View.VISIBLE);
+                    foodImage.setImageResource(R.drawable.like_icon_food);
+                    foodLayout.setBackgroundResource(R.drawable.round_back_home);
+
+
+                    ScaleAnimation scaleAnimation = new ScaleAnimation(0.8f,1.0f,1f,1f, Animation.RELATIVE_TO_SELF,1.0f,Animation.RELATIVE_TO_SELF,0.0f);
+                    scaleAnimation.setDuration(200);
+                    scaleAnimation.setFillAfter(true);
+                    foodLayout.startAnimation(scaleAnimation);
+
+
+                    selectedTab = 2;
+
+                }
+
             }
         });
 
         bmiLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(selectedTab != 3){
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.fragmeentContainer, BMIFragment.class, null)
+                            .commit();
+                    homeText.setVisibility(View.GONE);
+                    foodText.setVisibility(View.GONE);
+
+                    foodImage.setImageResource(R.drawable.icon_food);
+                    homeImage.setImageResource(R.drawable.icon_home);
+
+
+                    foodLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    homeLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+
+                    bmiText.setVisibility(View.VISIBLE);
+                    bmiImage.setImageResource(R.drawable.like_icon_bmi);
+                    bmiLayout.setBackgroundResource(R.drawable.round_back_home);
+
+
+                    ScaleAnimation scaleAnimation = new ScaleAnimation(0.8f,1.0f,1f,1f, Animation.RELATIVE_TO_SELF,0.0f,Animation.RELATIVE_TO_SELF,0.0f);
+                    scaleAnimation.setDuration(200);
+                    scaleAnimation.setFillAfter(true);
+                    bmiLayout.startAnimation(scaleAnimation);
+
+
+                    selectedTab = 3;
+
+                }
 
             }
         });
